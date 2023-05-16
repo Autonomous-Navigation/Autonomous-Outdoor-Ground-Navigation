46e50c33377b3eb506841d1f73373fb1fd2a7846
BhumilDepani <bhumil.depani1@gmail.com>
2023-05-16 15:43:44 -0700

    Restructuring the directory

--- test_ws/build/rplidar_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson-07/test_ws/src/rplidar_ros
-# Build directory: /home/jetson-07/test_ws/build/rplidar_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
